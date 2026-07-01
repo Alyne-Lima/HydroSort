2976ade2eea7a649ec84b4657245ca06b9311321
Alyne-Lima <alyne.santos.lima@gmail.com>
2026-07-01 10:51:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -65,10 +65,10 @@ section[data-testid="stSidebar"] *{
 </style>
 """, unsafe_allow_html=True)
 
-col1, col2 = st.columns([0.9,6])
+col1, col2 = st.columns([1.5,3])
 
 with col1:
-    st.image("logo_hydrosort.png", width=170)
+    st.image("logo_hydrosort.png", width=290)
 
 with col2:
     st.markdown(
